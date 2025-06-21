b18d63013041ce30403981d048992afac6a19332
Lzybear22 <167822796+Lzybear22@users.noreply.github.com>
2025-06-21 17:00:42 -0600

    added comments and added readme

---
+++ ReadMe.md
@@ -0,0 +1,35 @@
+# Overview
+
+{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+This program is a health and fitness tracker made with Python that is connected to a Amazon RDS database. The data is stored in the cloud instead of on your personal computer. With this program you can add workouts, log health information, view what you have logged and delete data if you want to. 
+
+The purpose of making this program is to keep track of diffrent workouts and health data. I wanted to make sure my data couldn't be lost and be accessed anywhere so I hosted a database in a Amazon RDS database in the cloud. This program also helped me learn how to connect a python program to a cloud database and use it to hold information.
+
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Cloud Database
+
+I am using Amazon's Relational Database Service(RDS) or my cloud database. This lets me run a database in the cloud so I can store data without running my own server and database. Also I can always access it on any computer.
+
+My database is called workout and has two tables. One is Workouts and it has user_id which is the id of the user, exercise_name which allows users to add the name of the workout. Duration which is how long the workout lasted. Weight which is how much weight you used in pounds. And finally workout_date which is the day the workout happend. The second table is health_data which has user_id to store who made the querys. Weight which is how much you weigh. Calories_burned which is how much calories you've burned. And finaly health_date which just tracks the day you added them.
+
+# Development Environment
+
+To make this program I used Python and connected it to a Amazon RDS database. Python is a object oriented program languge that can work with MySQL. For the database I used Amazon RDS lets me store data remotely. To connect my python program to RDS I used pymysql library. This lets my Python code to connect with MySQL databases. And I used datetime to record the dates of when the workouts were logged.
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+
+- AWS(https://docs.aws.amazon.com/rds/)
+- MySQLTutorial(https://www.mysqltutorial.org/)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+
+- Add user authentication to make data more secure
+- Add milestone data to automaticlly show off how you've done the past month
+- Make it so it can be used on phones so its more user friendly 
--- workout_app.py
+++ workout_app.py
@@ -1,54 +1,55 @@
-import pymysql
+import pymysql # Connecting and using MySQL database
 from datetime import date
 
-
+# AWS RDS database connection
 db_host="database-1.cyuht5zo7uqk.us-east-1.rds.amazonaws.com" 
 db_user="admin"
 db_pass="Lzybear123"
 db_name="workout"
 
+# Establish connection to the MySQL database
 connection = pymysql.connect(host = db_host, database = db_name, user = db_user, password = db_pass)
 print("Connected to the database")
-
+# Print MySQL server version to verify connection
 cursor = connection.cursor()
 cursor.execute('SELECT version()')
 db_version = cursor.fetchone()
 print(db_version)
 
 cursor.close()
-
+# Create a new cursor for queries
 cursor = connection.cursor()
-
-def log_workout():
+# Logs a new workout to the database
+def log_workout(): 
     user_id = int(input("User ID: "))
     exercise_name = input("Exercise name: ")
     duration = int(input("Duration (minutes): "))
     weight = float(input("Weight used (lbs): "))
     today = date.today()
-
+    # Adds workout data to the workout table
     cursor.execute("""
         INSERT INTO workouts (user_id, exercise_name, duration, weight, workout_date)
         VALUES (%s, %s, %s, %s, %s)
     """, (user_id, exercise_name, duration, weight, today))
     connection.commit()
     print("Workout logged.")
-
+# Logs daily health data
 def log_health_data():
     user_id = int(input("User ID: "))
     weight = float(input("Weight (lbs): "))
     calories_burned = int(input("Calories burned: "))
     today = date.today()
-
+    # Adds health data into the health_data table
     cursor.execute("""
         INSERT INTO health_data (user_id, weight, calories_burned, health_date)
         VALUES (%s, %s, %s, %s)
     """, (user_id, weight, calories_burned, today))
     connection.commit()
     print("Health data logged.")
-
+# Displays workouts and health data for a given user
 def view_user_data():
     user_id = int(input("Enter your User ID: "))
-
+    # Display all workouts for this user
     print("\nWorkouts:")
     cursor.execute("""
         SELECT exercise_name, duration, weight, workout_date
@@ -61,7 +62,7 @@ def view_user_data():
             print(f"Exercise: {w[0]}, Duration: {w[1]} min, Weight: {w[2]} lbs, Date: {w[3]}")
     else:
         print("No workouts found.")
-
+    # Display all health data for user
     print("\nHealth Data:")
     cursor.execute("""
         SELECT weight, calories_burned, health_date
@@ -74,7 +75,7 @@ def view_user_data():
             print(f"Weight: {h[0]} lbs, Calories Burned: {h[1]}, Date: {h[2]}")
     else:
         print("No health data found.")
-
+# Deletes a single workout or health record by ID
 def delete_data():
     print("\nDelete Menu")
     print("1. Delete Workout")
@@ -82,23 +83,23 @@ def delete_data():
     choice = input("Select an option: ")
 
     if choice == "1":
-        user_id = int(input("User ID: "))
+        user_id = int(input("User ID: ")) # User choose to delete workout
         cursor.execute("SELECT id, exercise_name, workout_date FROM workouts WHERE user_id = %s", (user_id,))
         records = cursor.fetchall()
 
         if not records:
             print("No workouts found.")
             return
-
+        # Display workout entries with id's
         print("\nWorkouts:")
         for rec in records:
             print(f"ID: {rec[0]}, Exercise: {rec[1]}, Date: {rec[2]}")
-
+        # User selects which one to delete
         delete_id = int(input("Enter the ID of the workout to delete: "))
         cursor.execute("DELETE FROM workouts WHERE id = %s AND user_id = %s", (delete_id, user_id))
         connection.commit()
         print("Workout deleted.")
-
+    # User chose to delete a health 
     elif choice == "2":
         user_id = int(input("User ID: "))
         cursor.execute("SELECT id, health_date FROM health_data WHERE user_id = %s", (user_id,))
@@ -107,24 +108,24 @@ def delete_data():
         if not records:
             print("No health records found.")
             return
-
+        # Display health records with id's
         print("\nHealth Records:")
         for rec in records:
             print(f"ID: {rec[0]}, Date: {rec[1]}")
-
+        # Choses which health entry to delete
         delete_id = int(input("Enter the ID of the health record to delete: "))
         cursor.execute("DELETE FROM health_data WHERE id = %s AND user_id = %s", (delete_id, user_id))
         connection.commit()
         print("Health record deleted.")
     else:
         print("Invalid choice.")
-
+# Clears all data in the workouts and health_data tables
 def clear_tables():
     cursor.execute("TRUNCATE TABLE workouts;")
     cursor.execute("TRUNCATE TABLE health_data;")
     connection.commit()
     print("All data cleared from workouts and health_data tables.")
-
+# Main program and menu table
 while True:
     print("\nHealth & Fitness Tracker")
     print("1. Log Workout")
@@ -149,7 +150,7 @@ while True:
         break
     else:
         print("Invalid choice. Please try again.")
-
+# Close database connection when done
 cursor.close()
 connection.close()
 print("Disconnected from the database.")
